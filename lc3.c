f4223d6eeb7ce4cb9116349b517e067099e48b99
Jianfeng Wang <me@jeffreywang.cn>
2022-03-10 22:19:58 +0800

    [lc3.c] op structure

--- lc3.c
+++ lc3.c
@@ -126,8 +126,97 @@ int main(int argc, const char* argv[]) {
 
     int running = 1;
     while (running) {
-        
+        /* FETCH */
+        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t op = instr >> 12;
+
+        switch (op)
+        {
+            case OP_ADD:
+                /* ADD */
+                {
+                    /* destination register (DR) */
+                    uint16_t r0 = (instr >> 9) & 0x7;
+                    uint16_t r1 = (instr >> 6)
+                }
+                break;
+            case OP_AND:
+                /* AND */
+                {
+                }
+                break;
+            case OP_NOT:
+                /* NOT */
+                {
+                }
+                break;
+            case OP_BR:
+                /* BR */
+                {
+                }
+                break;
+            case OP_JMP:
+                /* JMP */
+                {
+                }
+                break;
+            case OP_JSR:
+                /* JSR */
+                {
+                }
+                break;
+            case OP_LD:
+                /* LD */
+                {
+                }
+                break;
+            case OP_LDI:
+                /* LDI */
+                {
+                }
+                break;
+            case OP_LDR:
+                /* LDR */
+                {
+                }
+                break;
+            case OP_LEA:
+                /* LEA */
+                {
+                }
+                break;
+            case OP_ST:
+                /* ST */
+                {
+                }
+                break;
+            case OP_STI:
+                /* STI */
+                {
+                }
+                break;
+            case OP_STR:
+                /* STR */
+                {
+                }
+                break;
+            case OP_TRAP:
+                /* TRAP */
+                {
+                }
+                break;
+            case OP_RES:
+            case OP_RTI:
+            default:
+                /* BAD OPCODE */
+                abort();
+                break;
+
+        }
     }
+    /* Shutdown */
+    restore_input_buffering();
+
 }
 
 
